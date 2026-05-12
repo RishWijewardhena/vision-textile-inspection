fc358352f7b3f54c711e4ab40d6cbd4a626cd3f4
RishWijewardhena <rish2001wijewardhana@gmail.com>
2026-05-12 18:04:44 +0700

    fix(auto_capture.py) format code for consistency and readability

--- Utils/auto_capture.py
+++ Utils/auto_capture.py
@@ -1,15 +1,15 @@
-import  cv2
+import cv2
 import time
 import os
+from PIL import Image
 
-def capture_camera(save_dir="Photos",interval=2):
-    os.makedirs(save_dir,exist_ok=True)
+def capture_camera(save_dir="Photos", interval=2):
+    os.makedirs(save_dir, exist_ok=True)
 
     cap = cv2.VideoCapture("/dev/video0")
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 960)
     
-
     if not cap.isOpened():
         print("camera open failed")
         return
@@ -19,13 +19,16 @@ def capture_camera(save_dir="Photos",interval=2):
     try:
         count=1
         while True:
-            ret,frame=cap.read()
+            ret, frame = cap.read()
             if not ret:
                 print("Failed to capture frame")
                 break
             
-            filename=os.path.join(save_dir,f"photo_{count:03d}.jpg")
-            cv2.imwrite(filename,frame)
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            filename = os.path.join(save_dir, f"photo_{count:03d}.jpg")
+            
+            # Save as RGB using PIL
+            Image.fromarray(frame).save(filename)
 
             print(f"Saved {filename}")
 
